bc7b9d97bffaf5a6e03d59460a9a345f6f3d451f
vishwaashish <vishwakarmaashish165@gmail.com>
2024-02-29 01:31:03 +0530

    update editor

--- components/projects/codesnapshot/EditorContainer.tsx
+++ components/projects/codesnapshot/EditorContainer.tsx
@@ -1,32 +1,36 @@
-import { Suspense, forwardRef } from 'react'
+import { Suspense, forwardRef, memo, useEffect, useRef, useState } from 'react'
 import InfiniteViewer from 'react-infinite-viewer'
 import EditorContent from './EditorContent' // Assuming EditorContent is a separate component for editor content
 
 const EditorContainer = forwardRef<HTMLDivElement, any>((_props, ref) => {
+  const editorRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (editorRef.current) {
+      const height = editorRef.current.clientHeight + 200
+      const code = document.querySelector('.codesnapshotviewer')
+      if (code) {
+        code.setAttribute('style', `height: ${height}px`)
+      }
+    }
+  })
 
   return (
     <div className="flex justify-center items-center">
       <InfiniteViewer
-        className="viewer w-full h-[50vh] border-2  rounded-xl overflow-hidden border-y-8 "
+        className={`codesnapshotviewer viewer w-full  border-2  rounded-xl border-y-8  `}
         margin={0}
         threshold={100}
-        rangeX={[-5000, 5000]}
-        rangeY={[-5000, 5000]}
-        onScroll={e => {
-          console.log(e)
-        }}
-        onDrag={e => {
-          console.log(e)
-        }}
-        usePinch={true}
+        rangeX={[-2000, 2000]}
+        rangeY={[-2000, 2000]}
+        useAutoZoom={true}
+        useWheelScroll={true}
         displayHorizontalScroll={true}
         displayVerticalScroll={true}
       >
         <div className="viewport">
-          <div ref={ref} className="">
-            <Suspense fallback={<>Loading...</>}>
-              <EditorContent />
-            </Suspense>
+          <div ref={editorRef}>
+            <EditorContent ref={ref} />
           </div>
         </div>
       </InfiniteViewer>
--- components/projects/codesnapshot/EditorContent.tsx
+++ components/projects/codesnapshot/EditorContent.tsx
@@ -1,3 +1,5 @@
+import { forwardRef, memo, useCallback, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {
   selectCodeSnapShotState,
   setCode,
@@ -5,11 +7,9 @@ import {
   setHighlightedCode,
 } from '@/store/codesnapshotStore'
 import { ICodeSnapShort } from '@/types/codesnapshot.model'
-import { useCallback, useState, useEffect, memo } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import renderCode from './shikiRenderer'
 
-const EditorContent = () => {
+const EditorContent = forwardRef<HTMLDivElement, any>((prop, ref) => {
   const {
     code,
     language,
@@ -28,13 +28,14 @@ const EditorContent = () => {
 
   const textareaElement = useCallback(() => {
     const divElement = document.querySelector('.shikicontainer')
-    divElement &&
+    if (divElement) {
       dispatch(
         setEditorStyleWidthHeight({
           width: divElement.clientWidth,
           height: divElement.clientHeight,
         }),
       )
+    }
   }, [dispatch])
 
   const memoizedHighlightCode = useCallback(async () => {
@@ -46,10 +47,10 @@ const EditorContent = () => {
         lineNumber,
         editorHeader,
       )
-      highlighted &&
+      if (highlighted) {
         dispatch(setHighlightedCode({ code: highlighted, isLineNumber: true }))
-
-      textareaElement()
+        textareaElement()
+      }
     } catch (e) {
       console.error(e)
     }
@@ -67,10 +68,6 @@ const EditorContent = () => {
     memoizedHighlightCode()
   }, [memoizedHighlightCode])
 
-  //   useEffect(() => {
-  //     textareaElement()
-  //   }, [textareaElement])
-
   const handleChange = useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
       dispatch(setCode(e.target.value))
@@ -80,59 +77,65 @@ const EditorContent = () => {
 
   const handleKeyUp = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (event.ctrlKey && event.key === 'v') {
+      textareaElement()
       dispatch(setCode((event.target as HTMLInputElement).value))
     }
   }
 
   return (
     <div
-      className="relative w-min not-prose text-sm"
+      ref={ref}
+      className="relative w-min "
       style={{
-        padding: editorPadding + 'rem',
+        padding: `${editorPadding}rem`,
         backgroundColor: editorBackground.backgroundColor,
         backgroundImage: editorBackground.backgroundImage,
       }}
     >
       <div
-        className="relative overflow-hidden"
+        className="relative not-prose text-sm join join-vertical overflow-hidden"
         style={{
-          borderRadius: editorRadius + 'px',
+          borderRadius: `${editorRadius}px`,
         }}
       >
-        <div
-          className="flex h-10 w-full items-center justify-between gap-4 px-5"
-          style={{ backgroundColor: editorStyle.headerColor }}
-        >
-          <div className="grid h-full w-full items-center grid-cols-[60px_1fr_60px] gap-4">
-            <div className="flex items-center gap-2">
-              <div className="h-[13px] w-[13px] rounded-full bg-[#ff5f57]"></div>
-              <div className="h-[13px] w-[13px] rounded-full bg-[#febc2e]"></div>
-              <div className="h-[13px] w-[13px] rounded-full bg-[#28c840]"></div>
+        {editorHeader && (
+          <div
+            className="flex join-item h-10 w-full items-center justify-between gap-4 px-5"
+            style={{ backgroundColor: editorStyle.headerColor }}
+          >
+            <div className="grid h-full w-full items-center grid-cols-[60px_1fr_60px] gap-4">
+              <div className="flex items-center gap-2">
+                <div className="h-[13px] w-[13px] rounded-full bg-[#ff5f57]"></div>
+                <div className="h-[13px] w-[13px] rounded-full bg-[#febc2e]"></div>
+                <div className="h-[13px] w-[13px] rounded-full bg-[#28c840]"></div>
+              </div>
+              <div className="filename flex justify-center"></div>
+              <div></div>
             </div>
-            <div className="filename flex justify-center"></div>
-            <div></div>
           </div>
-        </div>
+        )}
         <div
-          className="px-5 py-3 shikicontainer leading-relaxed flex flex-row"
+          className="px-5 py-3 min-h-12 join-item shikicontainer leading-relaxed flex flex-row"
           style={{
             backgroundColor: editorStyle.backgroundColor,
           }}
         >
-          <div
-            className="flex flex-col items-end"
-            style={{ paddingRight: '1rem' }}
-            dangerouslySetInnerHTML={{
-              __html: editorlineNumberCode,
-            }}
-          />
+          {lineNumber && (
+            <div
+              className="flex flex-col items-end"
+              style={{ paddingRight: '1rem' }}
+              dangerouslySetInnerHTML={{
+                __html: editorlineNumberCode,
+              }}
+            />
+          )}
 
           <div className="relative w-full">
             <textarea
               style={{
                 fontFamily:
                   'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-                height: editorStyle.height +'px',
+                height: `${editorStyle.height}px`,
               }}
               className="shikitextarea overflow-hidden textarea p-0 left-0 top-0 textarea-bordered border-0 outline-0 focus:outline-0 caret-slate-100 text-transparent leading-relaxed resize-none w-full bg-transparent absolute"
               value={code}
@@ -152,6 +155,8 @@ const EditorContent = () => {
       </div>
     </div>
   )
-}
+})
 
-export default memo(EditorContent)
+EditorContent.displayName = 'EditorContent'
+
+export default EditorContent
